39a65b382efe977ecade3063118aa428cddd09cb
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-10-28 17:23:31 +0200

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import com.company.Employee.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim Buchalka", 21);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+        Employee red = new Employee("Red RidingHood", 35);
+        Employee charming = new Employee("Prince Charming", 31);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+        employees.add(red);
+        employees.add(charming);
+
+        System.out.println("Empoyees over 30");
+        System.out.println("================");
+        for (Employee employee : employees) {
+            if (employee.getAge() > 30) {
+                System.out.println(employee.getAge());
+            }
+        }
+    }
+}
